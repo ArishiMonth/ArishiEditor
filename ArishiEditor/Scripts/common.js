ad1eaba2fb9125d36d8de8abbc441e226af2c0b7
HuQian11 <hq_1314@163.com>
2017-07-20 10:37:32 +0800

    插件封装
    样式修改相关

--- ArishiEditor/Scripts/common.js
+++ ArishiEditor/Scripts/common.js
@@ -262,9 +262,7 @@ $dxh.common = {
 操作提示框封装（优化移动端下提示带网址问题）
 */
 (function ($) {
-
-    window.Ewin = function () {
-        var html = '<div id="[Id]" class="modal fade" role="dialog" aria-labelledby="modalLabel">' +
+    var html = '<div id="[Id]" class="modal fade" role="dialog" aria-labelledby="modalLabel">' +
            '<div class="modal-dialog modal-sm">' +
             '<div class="modal-content">' +
             '<div class="modal-header">' +
@@ -281,105 +279,65 @@ $dxh.common = {
             '</div>' +
            '</div>' +
            '</div>';
-
-
-        var dialogdHtml = '<div id="[Id]" class="modal fade" role="dialog" aria-labelledby="modalLabel">' +
-           '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-            '<div class="modal-header">' +
-             '<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>' +
-             '<h4 class="modal-title" id="modalLabel">[Title]</h4>' +
-            '</div>' +
-            '<div class="modal-body">' +
-            '</div>' +
-            '</div>' +
-           '</div>' +
-           '</div>';
-        var reg = new RegExp("\\[([^\\[\\]]*?)\\]", 'igm');
-        var generateId = function () {
-            var date = new Date();
-            return 'mdl' + date.valueOf();
+    var reg = new RegExp("\\[([^\\[\\]]*?)\\]", 'igm');
+    var generateId = function () {
+        var date = new Date();
+        return 'mdl' + date.valueOf();
+    }
+    var init = function (options) {
+        options = $.extend({}, {
+            title: "提示",
+            message: "提示内容",
+            btnok: "确定",
+            btncl: "取消",
+            width: 200,
+            auto: false
+        }, options || {});
+        var modalId = generateId();
+        var content = html.replace(reg, function (node, key) {
+            return {
+                Id: modalId,
+                Title: options.title,
+                Message: options.message,
+                BtnOk: options.btnok,
+                BtnCancel: options.btncl
+            }[key];
+        });
+        $('body').append(content);
+        $('#' + modalId).modal({
+            width: options.width,
+            backdrop: 'static'
+        });
+        $('#' + modalId).on('hide.bs.modal', function (e) {
+            $('body').find('#' + modalId).remove();
+        });
+        return modalId;
+    }
+    window.alert = function (options) {
+        if (typeof options == 'string') {
+            options = {
+                message: options
+            };
         }
-        var init = function (options) {
-            options = $.extend({}, {
-                title: "提示",
-                message: "提示内容",
-                btnok: "确定",
-                btncl: "取消",
-                width: 200,
-                auto: false
-            }, options || {});
-            var modalId = generateId();
-            var content = html.replace(reg, function (node, key) {
-                return {
-                    Id: modalId,
-                    Title: options.title,
-                    Message: options.message,
-                    BtnOk: options.btnok,
-                    BtnCancel: options.btncl
-                }[key];
-            });
-            $('body').append(content);
-            $('#' + modalId).modal({
-                width: options.width,
-                backdrop: 'static'
-            });
-            $('#' + modalId).on('hide.bs.modal', function (e) {
-                $('body').find('#' + modalId).remove();
-            });
-            return modalId;
-        }
+        var id = init(options);
+        var modal = $('#' + id);
+        modal.find('.ok').removeClass('btn-success').addClass('btn-primary');
+        modal.find('.cancel').hide();
 
         return {
-            alert: function (options) {
-                if (typeof options == 'string') {
-                    options = {
-                        message: options
-                    };
+            id: id,
+            on: function (callback) {
+                if (callback && callback instanceof Function) {
+                    modal.find('.ok').click(function () { callback(true); });
                 }
-                var id = init(options);
-                var modal = $('#' + id);
-                modal.find('.ok').removeClass('btn-success').addClass('btn-primary');
-                modal.find('.cancel').hide();
-
-                return {
-                    id: id,
-                    on: function (callback) {
-                        if (callback && callback instanceof Function) {
-                            modal.find('.ok').click(function () { callback(true); });
-                        }
-                    },
-                    hide: function (callback) {
-                        if (callback && callback instanceof Function) {
-                            modal.on('hide.bs.modal', function (e) {
-                                callback(e);
-                            });
-                        }
-                    }
-                };
             },
-            confirm: function (options) {
-                var id = init(options);
-                var modal = $('#' + id);
-                modal.find('.ok').removeClass('btn-primary').addClass('btn-success');
-                modal.find('.cancel').show();
-                return {
-                    id: id,
-                    on: function (callback) {
-                        if (callback && callback instanceof Function) {
-                            modal.find('.ok').click(function () { callback(true); });
-                        }
-                    },
-                    hide: function (callback) {
-                        if (callback && callback instanceof Function) {
-                            modal.on('hide.bs.modal', function (e) {
-                                callback(e);
-                            });
-                            modal.find('.cancel').click(function () { callback(false); });
-                        }
-                    }
-                };
+            hide: function (callback) {
+                if (callback && callback instanceof Function) {
+                    modal.on('hide.bs.modal', function (e) {
+                        callback(e);
+                    });
+                }
             }
-        }
-    }();
+        };
+    };
 })(jQuery);
--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ $(function(){
         imgTag:"imgModal",
         imgUrl:"/UEditorController.ashx?action=uploadimage",
         content:'',
-        cssStyleId:"#editorCss"
+        cssStyleId:"#editorCss",
+        height:window.innerHeight - $("#footer").innerHeight()-20,
+        resizeHandler:function(){
+            var that = this;
+            var height = window.innerHeight;
+            window.onresize = function (e) {
+                editor.setContentHeight(window.innerHeight - $("#footer").innerHeight()-20);
+                var opt = that.getElementOffset( editor.options.node ==null?$("#content")[0].lastChild:editor.options.node);
+                if (window.innerHeight < height-30) {
+                    $("#content").scrollTop(opt.top-50);
+                }
+            }
+        }
     });
     console.log(editor);
-    editor.setContentHeight=function(){
-        var $height = window.innerHeight - $("#footer").innerHeight() - 20;
-        $("#content").css("height", $height + "px");
-    };
+   //未添加图片后台时，建议编辑器中只有一张图片时查看getAllHtml方法是否正确，因为base64位字符串过多，在手机中查看容易导致浏览器崩溃
     $(document).on("click","#btn_Save",function(){
         var $html = editor.getAllHtml();
         alert($html);
